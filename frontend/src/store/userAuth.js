f71639122b270e4164943034f035d343416a023b
CHANDRASEKHAR ARIPAKA <106866978+SekharChandra21@users.noreply.github.com>
2025-07-07 10:37:51 +0530

    Updated socket base url userAuth.js

--- frontend/src/store/userAuth.js
+++ frontend/src/store/userAuth.js
@@ -85,7 +85,7 @@ export const useAuthStore = create((set, get) => ({
     const { authUser } = get();
     if (!authUser || get().socket?.connected) return;
 
-    const socket = io(BASE_URL, {
+    const socket = io('/', {
       query: {
         userId: authUser._id,
       },
